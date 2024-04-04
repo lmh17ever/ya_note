bb3c6f1d5ca65914d2df3b995602c54d0e3ac0a6
stayasobak <dug-danya@yandex.ru>
2024-04-04 23:29:26 +0300

    all tests passed

--- notes/pytest_tests/test_content.py
+++ notes/pytest_tests/test_content.py
@@ -6,7 +6,7 @@ from notes.forms import NoteForm
 
 
 @pytest.mark.parametrize(
-    'parametrized_client, note_int_list',
+    'parametrized_client, note_in_list',
     (
         (pytest.lazy_fixture('author_client'), True),
         (pytest.lazy_fixture('not_author_client'), False),
@@ -15,7 +15,7 @@ from notes.forms import NoteForm
 def test_note_list_for_different_users(
     note, parametrized_client, note_in_list
 ):
-    url = reverse('note:list')
+    url = reverse('notes:list')
     response = parametrized_client.get(url)
     object_list = response.context['object_list']
     assert (note in object_list) is note_in_list
